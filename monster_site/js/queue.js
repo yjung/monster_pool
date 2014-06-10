8997f5d945a76238704e6eb320aa290773881b55
jaegermathias <jaegermathias@googlemail.com>
2014-06-10 14:47:48 +0200

    Bibliotheken aktualisiert, Austausch minifizierter version gegen vollstaendigen Build von Three.js, Erster funktionierender Prototyp zur Steuerung mit Physik-Objekten.

--- monster_site/js/queue.js
+++ monster_site/js/queue.js
@@ -1,24 +1,23 @@
 // Queue einladen und initialisieren
 function queueLaden() {
-    var ColladaLoader = new THREE.ColladaLoader();
-    // JSON-Loader erstellen
-    ColladaLoader.load('../assets/dae/queue_a02.dae', function (collada) {
+    var ColladaLoader = new THREE.ColladaLoader();                          // Collada-Loader erstellen
+    ColladaLoader.load('../assets/dae/queue_a02.dae', function (collada) {  // Einladen der Datei mit Referenz "collada"
 
-        /**/
-        var dae = collada.scene.children[0];                    // Referenz auf Objekt 1 der eingeladenen .dae-Szene
-        dae.updateMatrix();                                     // Matrix nach Extraktion aus .dae-Szene aktualisieren
+        /* Geometrie aus der .dae-Szene extrahieren*/
+        var daeGeometrie = collada.scene.children[0].children[0].geometry;  // Referenz auf Geometrie von Objekt 1 der eingeladenen .dae-Szene
 
         /*Physikalischer Queue mit Physi.js*/
-//        queueMaterial = Physijs.createMaterial(lGreenT, 0, 0);    // Physi.js-Material initialisieren
-//        game.queue = new Physijs.BoxMesh(collada.scene.children[0].geometry, queueMaterial, 0); // Objekterzeugung
-//        game.queue._physijs.collision_flags = 4; 		// COLLISION IST ZUM BUGFIXING NOCH DEAKTIVIERT
+        var queueMaterial = Physijs.createMaterial(lGreenT, 0, 0);          // Physi.js-Material initialisieren
+        game.queue = new Physijs.CylinderMesh(daeGeometrie, queueMaterial, 0);   // Objekterzeugung
+        game.queue._physijs.collision_flags = 1; 		                    // COLLISION IST ZUM BUGFIXING NOCH DEAKTIVIERT
 
+        game.queue.useQuaternion = true;
 
-        /* Nicht physikalischer Queue mit Three.js*/
-        game.queue = new THREE.Mesh(dae.geometry, lGreenT);     // Objekterzeugng
+        game.queue.rotation.copy(game.camera.rotation);
 
+        console.log(game.queue);
 
-        game.szene.add(game.queue);                             // Queue zur Szene hinzufuegen
+        game.szene.add(game.queue);                                         // Queue zur Szene hinzufuegen
     });
 };
 
@@ -28,7 +27,18 @@ function queueAktualisieren() {
     // Rotationsmodus
     if (game.state === game.modus.orbitrotation) {
 
+        console.log(game.queue);
+        game.queue.__dirtyRotation = true;
+        game.queue.__dirtyPosition = true;
         game.orbitControls.Mittelpunkt.copy(game.whiteBall.position); // Mittelpunkt der Orbitrotation auf der Kugel
+        game.queue.__dirtyPosition = true;
+        game.queue.__dirtyRotation = true;
+
+        game.queue.__dirtyRotation = true;
+        game.queue.__dirtyPosition = true;
+        game.queue.rotation.copy(game.camera.rotation);
+        game.queue.__dirtyRotation = true;
+        game.queue.__dirtyPosition = true;
 
         /* Lokale Variablen zur Positionsberechnung initialisieren*/
         var richtungCam = new THREE.Vector3();                  // Vektor fuer Richtung initialisieren
@@ -38,6 +48,7 @@ function queueAktualisieren() {
 
         /* Positionen der Objekte in Vektorvariablen sichern.*/
         positionCam.copy(game.camera.position);                 // Position Kamera sichern
+        positionCam.y -= 1;
         positionBall.copy(game.whiteBall.position);             // Position Kugel sichern
 
         /* Richtungsvektor fuer Queuepositionierung bestimmen*/
@@ -45,7 +56,7 @@ function queueAktualisieren() {
         richtungCam.normalize();                                // Richtung zur Kamera normalisiert
 
         /* Sicherheitsabstand zur Kugel*/
-        richtungCam.multiplyScalar(15);                         // Verlaengerung des Normalisierten Richtungsvektors
+        richtungCam.multiplyScalar(5);                         // Verlaengerung des Normalisierten Richtungsvektors
 
         game.queue.__dirtyRotation = true;
         game.queue.__dirtyPosition = true;
@@ -61,30 +72,40 @@ function queueAktualisieren() {
         game.queue.__dirtyPosition = true;
 
 
-        var qZiel = new THREE.Quaternion();
-        game.queue.__dirtyRotation = true;
-        game.queue.__dirtyPosition = true;
-        var qAusgang = game.queue.rotation._quaternion;
-        game.queue.__dirtyRotation = true;
-        game.queue.__dirtyPosition = true;
+//        game.queue.__dirtyRotation = true;
+//        game.queue.__dirtyPosition = true;
+//        richtungCam.normalize();
+//        game.queue.rotation.copy(game.camera.rotation);
+//        game.queue.__dirtyRotation = true;
+//        game.queue.__dirtyPosition = true;
 
-        game.queue.__dirtyRotation = true;
-        game.queue.__dirtyPosition = true;
-        game.queue.rotation._quaternion.copy(game.camera.rotation._quaternion);
-//        THREE.Quaternion.slerp(qAusgang, game.camera.rotation._quaternion, qZiel, 1 * game.clock.getDelta());
-        game.queue.__dirtyRotation = true;
-        game.queue.__dirtyPosition = true;
 
-        game.queue.__dirtyRotation = true;
-        game.queue.__dirtyPosition = true;
-//        game.queue.rotation._quaternion.copy(qZiel);
-        game.queue.__dirtyRotation = true;
-        game.queue.__dirtyPosition = true;
+
+//        var qZiel = new THREE.Quaternion();
+//        game.queue.__dirtyRotation = true;
+//        game.queue.__dirtyPosition = true;
+//        var qAusgang = game.queue.quaternion;
+//        game.queue.__dirtyRotation = true;
+////
+//        game.queue.__dirtyRotation = true;
+//        game.queue.__dirtyPosition = true;
+//        game.queue.__dirtyPosition = true;
+//        THREE.Quaternion.slerp(qAusgang, game.camera.quaternion, qZiel, 10 * game.clock.getDelta());
+//        game.queue.__dirtyRotation = true;
+//        game.queue.__dirtyPosition = true;
+////
+//        game.queue.__dirtyRotation = true;
+//        game.queue.__dirtyPosition = true;
+//        game.queue.quaternion.copy(qZiel);
+//        game.queue.__dirtyRotation = true;
+//        game.queue.__dirtyPosition = true;
+
+        // ------------------------------------------------------------
     }
 
-    // ------------------------------------------------------------
-
     if (game.state === game.modus.statisch) {
+        console.log(game.queue._physijs.collision_flags);
+//        game.queue._physijs.collision_flags = 2;
         var richtungsvektor = new THREE.Vector3();
         richtungsvektor.subVectors(game.queue.position, game.whiteBall.position);
 
@@ -94,13 +115,13 @@ function queueAktualisieren() {
 //
 //        console.log(kamerablick);
 
-        var zRichtung = new THREE.Vector3(0,0,1);
+        var zRichtung = new THREE.Vector3(0, 0, 1);
 
         game.queue.__dirtyRotation = true;
         game.queue.__dirtyPosition = true;
 //        game.queue.translateOnAxis(richtungsvektor, game.mausPosition.y_d * 20);
 //		game.queue.translateOnAxis(game.camera.rotation, game.mausPosition.y_d * 10);
-        game.queue.translateOnAxis (zRichtung, (game.mausPosition.y_d + game.mausPosition.x_d) * -10);
+        game.queue.translateOnAxis(zRichtung, (game.mausPosition.y_d + game.mausPosition.x_d) * -10);
         game.queue.__dirtyRotation = true;
         game.queue.__dirtyPosition = true;
     }
--- monster_site/js/whiteball.js
+++ monster_site/js/whiteball.js
@@ -1,7 +1,17 @@
 // Weisse Kugel aus physikalischem Grundobjekt
 function createWhiteBall(x,y,z) {
+
+    var friction = .2;      // low friction
+    var restitution = 0;   // low restitution
+
+    var material = Physijs.createMaterial(
+        new THREE.MeshBasicMaterial({ color: 0xffffff }),
+        friction,
+        restitution
+    );
+
     // Grundobjekterzeugung mit Material aus angelegter Bibliothek
-    game.whiteBall = new Physijs.SphereMesh(new THREE.SphereGeometry(1, 16, 16), pWhiteWireframeT, 100);
+    game.whiteBall = new Physijs.SphereMesh(new THREE.SphereGeometry(1, 16, 16), material, 1000);
     // Initiale Positionierung in der Welt
     game.whiteBall.position.x = x;
     game.whiteBall.position.y = y;
