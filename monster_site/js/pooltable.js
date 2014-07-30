4e7d2c2f5a260e35e864c8f3a2fc7601c0267dbc
jaegermathias <jaeger.mathias@googlemail.com>
2014-07-30 17:43:32 +0200

    MJ_A03: Entfernen nicht mehr benoetigter Dateien und Einsortieren neuer in Ordnerstrukturen.

--- monster_site/js/pooltable.js
+++ monster_site/js/pooltable.js
@@ -1,25 +1,9 @@
-// // Tisch einladen und initialisieren
-// function tischLaden() {
-// var JSONloader = new THREE.JSONLoader();        // JSON-Loader erstellen
-// JSONloader.load('../assets/json/snooker_a01.js', function(geometry, mat) {
-// game.tisch = new THREE.Mesh(geometry, mat[0]);
-// game.tisch.scale.x = .5;        // (Noch IM 3D-MODELL AENDERN!)
-// game.tisch.scale.y = .5;        // (Noch IM 3D-MODELL AENDERN!)
-// game.tisch.scale.z = .5;        // (Noch IM 3D-MODELL AENDERN!)
-//
-// game.tisch.position.x = -30;    // (Noch IM 3D-MODELL AENDERN!)
-// game.tisch.position.z = 15;     // (Noch IM 3D-MODELL AENDERN!)
-//
-// game.szene.add(game.tisch);     // Tisch zur Szene hinzufuegen
-// }, '../assets/tex/');               // Verweis auf Verzeichnis der Texturen
-// };
-
 // Tisch einladen und initialisieren
-function tischLaden()/* Tisch falsch rotiert (steht hochkant): Gedreht beine fehlen (Export error oder altes File¿)*/
+function ladePooltable()/* Tisch falsch rotiert (steht hochkant): Gedreht beine fehlen (Export error oder altes File¿)*/
 {
 	var ColladaLoader = new THREE.ColladaLoader();
 	// JSON-Loader erstellen
-	ColladaLoader.load('assets/dae/pooltable_a04.dae', function(collada) {
+	ColladaLoader.load('assets/dae/pooltable.dae', function(collada) {
 
 		var modelScene = collada.scene;
 		var tischbestandteile = 6; 						// Aktuell besteht das Tischmodell aus 7 Einzelteilen 
