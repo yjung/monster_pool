6f61e066e7599223c0904679162988b740314076
Juan Henao <juanhenao10@gmail.com>
2014-08-13 16:01:01 +0200

    JH_F02
    Sound fuer:
    -Ball-Ball Collision
    -Queue-Stoss
    
    #44

--- monster_site/js/kugeln.js
+++ monster_site/js/kugeln.js
@@ -13,13 +13,22 @@ function ladeKugeln(){
 		var kugelMaterial = modelScene.children[i].children[0].material;
 		
 		var kugel = new Physijs.SphereMesh(kugelGeometrie, Physijs.createMaterial((kugelMaterial),	0.1, 1.0),100);
-
+		kugel.name="ball-"+i;
 		// kugel.scale.set(0.25,0.25,0.25);
 		kugel.position.x = kugelPosition.x * 0.25;
 		kugel.position.y = kugelPosition.y * 0.25;
 		kugel.position.z = kugelPosition.z * 0.25;
 // 
 		game.szene.add(kugel);
+		
+			kugel.addEventListener('collision', function(object) {
+				patron= new RegExp("^ball"); //Pattern: The name starts with ball
+				console.log("Comparacion con "+object.name+": "+patron.test(object.name));
+				if((object === game.whiteBall)||(patron.test(object.name))){
+					soundEffekt("ball-ball");
+				}					
+			  
+		    });
 		}
 	});
 };
