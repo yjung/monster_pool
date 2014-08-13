dbce45f579510bf14de687d63d367dad2ca66c4c
jaegermathias <jaeger.mathias@googlemail.com>
2014-08-13 08:24:54 +0200

    MJ_F21: Asset-Erstellung: Rampe mit zusaetzlichen Flaechen fuer Physi.js-Plane-Meshes als Kollisionsflaechen. Entfernen von Vorgaengermodell. closed #93

--- monster_site/js/rampe.js
+++ monster_site/js/rampe.js
@@ -4,7 +4,7 @@ function erstelleRampe(loader, xPosition, yPosition, zPosition) {
 
 
 	// Slider einladen
-	ColladaLoader.load('assets/dae/rampeN.dae', function(collada) {
+	ColladaLoader.load('assets/dae/rampe.dae', function(collada) {
 
 		var modelScene = collada.scene.children[0].children[0];
 		var modelGeometry = modelScene.geometry;
